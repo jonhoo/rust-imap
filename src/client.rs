735ba9562a565a7fcd94ae281df84914356c134d
Matt McCoy <mattnenterprise@yahoo.com>
2016-06-21 20:06:37 -0400

    Adding test for RENAME command

--- src/client.rs
+++ src/client.rs
@@ -381,6 +381,19 @@ mod tests {
 	}
 
 	#[test]
+	fn rename() {
+		// TODO Make sure the response was read correctly
+		let response = b"a1 OK RENAME completed\r\n".to_vec();
+		let current_mailbox_name = "INBOX";
+		let new_mailbox_name = "NEWINBOX";
+		let command = format!("a1 RENAME {} {}\r\n", current_mailbox_name, new_mailbox_name);
+		let mock_stream = MockStream::new(response);
+		let mut client = create_client_with_mock_stream(mock_stream);
+		client.rename(current_mailbox_name, new_mailbox_name).unwrap();
+		assert!(client.stream.written_buf == command.as_bytes().to_vec(), "Invalid rename command");
+	}
+
+	#[test]
 	fn subscribe() {
 		// TODO Make sure the response was read correctly
 		let response = b"a1 OK SUBSCRIBE completed\r\n".to_vec();
