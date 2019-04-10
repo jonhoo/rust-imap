bfe7f130c55e696a16347bf7f6d7de7c21e673bf
Baudouin Feildel <baudouin.feildel@st.com>
2019-04-10 15:08:32 +0200

    Fix lint error

--- src/types/fetch.rs
+++ src/types/fetch.rs
@@ -119,7 +119,7 @@ impl Fetch {
             })
             .next()
     }
-    
+
     /// Extract the `INTERNALDATE` of a `FETCH` response
     ///
     /// See [section 2.3.3 of RFC 3501](https://tools.ietf.org/html/rfc3501#section-2.3.3) for
