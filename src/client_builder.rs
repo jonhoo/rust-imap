3f798dfaccf01e50237d2bb0bd24f0ea96c7c4f9
Todd Mortimer <todd@opennet.ca>
2021-05-06 18:49:40 -0400

    Add custom connect() function to handle TLS initialization.
    Use it for native and rustls initialization.

--- src/client_builder.rs
+++ src/client_builder.rs
@@ -1,4 +1,5 @@
 use crate::{Client, Result};
+use std::io::{Read, Write};
 use std::net::TcpStream;
 
 #[cfg(feature = "tls")]
@@ -70,37 +71,68 @@ where
     #[cfg(feature = "tls")]
     #[cfg_attr(docsrs, doc(cfg(feature = "tls")))]
     pub fn native_tls(&mut self) -> Result<Client<TlsStream<TcpStream>>> {
-        let tcp = TcpStream::connect((self.domain.as_ref(), self.port))?;
-        if self.starttls {
-            let mut client = Client::new(tcp);
-            client.read_greeting()?;
-            client.run_command_and_check_ok("STARTTLS")?;
+        self.connect(|domain, tcp| {
             let ssl_conn = TlsConnector::builder().build()?;
-            let tls = TlsConnector::connect(&ssl_conn, self.domain.as_ref(), client.into_inner()?)?;
-            Ok(Client::new(tls))
-        } else {
-            let ssl_conn = TlsConnector::builder().build()?;
-            let tls = TlsConnector::connect(&ssl_conn, self.domain.as_ref(), tcp)?;
-            Ok(Client::new(tls))
-        }
+            Ok(TlsConnector::connect(&ssl_conn, domain, tcp)?)
+        })
     }
 
     /// Return a new [`Client`] using `rustls` transport.
     #[cfg(feature = "rustls-tls")]
     #[cfg_attr(docsrs, doc(cfg(feature = "rustls-tls")))]
     pub fn rustls(&mut self) -> Result<Client<RustlsStream<TcpStream>>> {
-        let tcp = TcpStream::connect((self.domain.as_ref(), self.port))?;
-        if self.starttls {
+        self.connect(|domain, tcp| {
+            let ssl_conn = RustlsConnector::new_with_native_certs()?;
+            Ok(ssl_conn.connect(domain, tcp)?)
+        })
+    }
+
+    /// Make a [`Client`] using a custom TLS initialization. This function is intended
+    /// to be used if your TLS setup requires custom work such as adding private CAs
+    /// or other specific TLS parameters.
+    ///
+    /// The `handshake` argument should accept two parameters:
+    ///
+    /// - domain: [`&str`]
+    /// - tcp: [`TcpStream`]
+    ///
+    /// and yield a `Result<C>` where `C` is `Read + Write`. It should only perform
+    /// TLS initialization over the given `tcp` socket and return the encrypted stream
+    /// object, such as a [`native_tls::TlsStream`] or a [`rustls_connector::TlsStream`].
+    ///
+    /// If the caller is using `STARTTLS` and previously called [`starttls`](Self::starttls)
+    /// then the `tcp` socket given to the `handshake` function will be connected and will
+    /// have initiated the `STARTTLS` handshake.
+    ///
+    /// ```no_run
+    /// # use imap::ClientBuilder;
+    /// # use rustls_connector::RustlsConnector;
+    /// # fn main() -> Result<(), imap::Error> {
+    /// let client = ClientBuilder::new("imap.example.com", 993)
+    ///     .starttls()
+    ///     .connect(|domain, tcp| {
+    ///         let ssl_conn = RustlsConnector::new_with_native_certs()?;
+    ///         Ok(ssl_conn.connect(domain, tcp)?)
+    ///     })?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn connect<F, C>(&mut self, handshake: F) -> Result<Client<C>>
+    where
+        F: FnOnce(&str, TcpStream) -> Result<C>,
+        C: Read + Write,
+    {
+        let tcp = if self.starttls {
+            let tcp = TcpStream::connect((self.domain.as_ref(), self.port))?;
             let mut client = Client::new(tcp);
             client.read_greeting()?;
             client.run_command_and_check_ok("STARTTLS")?;
-            let ssl_conn = RustlsConnector::new_with_native_certs()?;
-            let tls = ssl_conn.connect(self.domain.as_ref(), client.into_inner()?)?;
-            Ok(Client::new(tls))
+            client.into_inner()?
         } else {
-            let ssl_conn = RustlsConnector::new_with_native_certs()?;
-            let tls = ssl_conn.connect(self.domain.as_ref(), tcp)?;
-            Ok(Client::new(tls))
-        }
+            TcpStream::connect((self.domain.as_ref(), self.port))?
+        };
+
+        let tls = handshake(self.domain.as_ref(), tcp)?;
+        Ok(Client::new(tls))
     }
 }
